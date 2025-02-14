888a4d97e014ed39926e836e99404ad60dd59152
lussuosoo <kristibedrina@gmail.com>
2025-02-14 19:01:44 +0300

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,6 +6,9 @@ int add(int a, int b) {
     return a + b;
 }
 
+int multiply(int a, int b) {
+    return a * b;
+}
 
 void printMessage(const std::string& message) {
     std::cout << message << std::endl;
