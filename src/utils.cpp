aad20f5caeca008317351fadf559ccd67b7d5cdc
lussuosoo <kristibedrina@gmail.com>
2025-02-14 18:11:51 +0300

    utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,12 @@
+#include "utils.h"
+#include <iostream>
 
+
+int add(int a, int b) {
+    return a + b;
+}
+
+
+void printMessage(const std::string& message) {
+    std::cout << message << std::endl;
+}
