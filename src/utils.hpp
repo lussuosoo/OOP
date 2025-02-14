b995eea6d5c0e4ead4ae0f01a9e738566452a5a0
lussuosoo <kristibedrina@gmail.com>
2025-02-14 19:01:09 +0300

    Update utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,8 +1,10 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <string>
+
 int add(int a, int b);
+int multiply(int a, int b);
 void printMessage(const std::string& message);
 
 #endif // UTILS_HPP
-
